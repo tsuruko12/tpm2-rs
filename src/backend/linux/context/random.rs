6a6199c857e943c41e9c80821358193fca148252
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-06-30 17:29:16 +0900

    refactor: change the return value for get_random_once

--- src/backend/linux/context/random.rs
+++ src/backend/linux/context/random.rs
@@ -1,13 +1,14 @@
+use tss_esapi::structures::Digest;
+
 use crate::error::{Error, Result};
 
 use super::Context;
 
 impl Context {
-    pub(crate) fn get_random_once(&mut self, num_bytes: usize) -> Result<Vec<u8>> {
+    pub(crate) fn get_random_once(&mut self, num_bytes: u16) -> Result<Digest> {
         self
             .ctx
-            .get_random(num_bytes)
-            .map(|digest| digest.value().to_vec())
+            .get_random(num_bytes as usize)
             .map_err(Error::from_tss_err)
     }
 }
