1b2eae40208290e9edc20fe89e94c950c46d19fd
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-07-03 20:09:38 +0900

    chore: clean up re-export path

--- src/backend/windows/types.rs
+++ src/backend/windows/types.rs
@@ -1,9 +1,26 @@
+mod algorithm;
+mod attribute;
+mod capability;
+mod ecc;
+mod handle;
+mod list;
+mod policy;
 mod response_code;
+mod sized;
+mod tag;
 mod tpm;
-mod tpm2b;
 
+pub(super) use algorithm::TpmAlgId;
+pub(super) use attribute::TpmaCc;
+pub(super) use capability::CapabilityData;
+pub(super) use ecc::TpmEccCurve;
+pub(super) use handle::TpmHandle;
+pub(super) use policy::TpmsPcrSelection;
 pub(super) use response_code::*;
+pub(super) use tag::{TpmsTaggedPcrSelect, TpmsTaggedProperty};
 pub(super) use tpm::*;
-pub(super) use tpm2b::*;
 
-use super::wire::unmarshal_tpm2b;
+use super::codec::{
+    unmarshal_algs, unmarshal_cc, unmarshal_cca, unmarshal_ecc_curves, unmarshal_handles,
+    unmarshal_pcr_properties, unmarshal_pcrs, unmarshal_tpm_properties,
+};
