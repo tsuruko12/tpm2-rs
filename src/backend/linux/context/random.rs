a2e3b9e059be659d9ba1fd60c9bcc37032cee282
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-07-03 20:04:27 +0900

    refactor: delete unnecessary import path

--- src/backend/linux/context/random.rs
+++ src/backend/linux/context/random.rs
@@ -1,14 +1,13 @@
-use tss_esapi::structures::Digest;
-
 use crate::error::{Error, Result};
 
 use super::Context;
 
 impl Context {
-    pub(crate) fn get_random_once(&mut self, num_bytes: u16) -> Result<Digest> {
+    pub(crate) fn get_random_once(&mut self, num_bytes: u16) -> Result<Vec<u8>> {
         self
             .ctx
             .get_random(num_bytes as usize)
+            .map(|digest| digest.value().to_vec())
             .map_err(Error::from_tss_err)
     }
 }
--- src/backend/windows.rs
+++ src/backend/windows.rs
@@ -1,7 +1,7 @@
+mod codec;
 mod commands;
 mod context;
 mod error;
 mod types;
-mod wire;
 
 pub use context::Context;
