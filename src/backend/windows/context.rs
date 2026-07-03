a91e21440fe4e5f1abc11a9bbd50d5c5aee0a60d
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-07-03 20:07:37 +0900

    chore: add import path

--- src/backend/windows/context.rs
+++ src/backend/windows/context.rs
@@ -1,11 +1,16 @@
+mod capability;
 mod random;
 mod tbs;
 
 use std::{ffi::c_void};
 
 use super::{
-    commands::{Command, CommandHeader, TPM_CC_GET_RANDOM, TPM_HEADER_SIZE},
-    types::{Digest, TpmRc, TpmSt, Uint32, TPM_RC_SUCCESS},
+    codec::marshal_be,
+    commands::{
+        Command, CommandHeader, TPM_CC_GET_CAPABILITY, TPM_CC_GET_RANDOM, TPM_HEADER_SIZE
+    },
+    types::{CapabilityData, TPM_RC_SUCCESS, TpmRc, TpmSt},
+    codec::{require_len, unmarshal_tpm2b},
 };
 
 type ContextHandle = *mut c_void;
