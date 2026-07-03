330e5d5183c7fde5c9d6290d98b7727b4c28bd82
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-07-03 20:10:22 +0900

    chore: add const

--- src/backend/windows/commands/command_code.rs
+++ src/backend/windows/commands/command_code.rs
@@ -1,3 +1,4 @@
 use super::TpmCc;
 
-pub(crate) const TPM_CC_GET_RANDOM: TpmCc =  0x0000_017B;
+pub(crate) const TPM_CC_GET_RANDOM: TpmCc = 0x0000_017B;
+pub(crate) const TPM_CC_GET_CAPABILITY: TpmCc = 0x0000_017A;
