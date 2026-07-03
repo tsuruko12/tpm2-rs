731fe677ae42f48ffcd8475ef5a64e45c1b6ed68
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-07-03 20:07:57 +0900

    chore: delete const

--- src/backend/windows/error.rs
+++ src/backend/windows/error.rs
@@ -8,8 +8,6 @@ use windows_sys::Win32::Foundation::{
 use super::types::*;
 use crate::error::Error;
 
-const TPM_RC_FMT1_E_MASK: TpmRc = 0x3F;
-
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum TbsError {
     #[error("TBS service is disabled")]
@@ -72,7 +70,7 @@ impl fmt::Display for TpmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::ResponseCode(rc) => {
-                write!(f, "TPM returned response code {rc:#05X}")
+                write!(f, "TPM returned response code {:#05X}", rc.raw())
             }
         }
     }
@@ -82,35 +80,28 @@ impl StdError for TpmError {}
 
 impl Error {
     pub(crate) fn from_rc(rc: TpmRc) -> Self {
-        debug_assert_ne!(rc, 0);
-
         let source = TpmError::ResponseCode(rc);
-        let base = base_rc(rc);
+        let base = rc.base();
 
         match base {
-            TPM_RC_BINDING 
-            | TPM_RC_KEY 
-            | TPM_RC_SIGN_CONTEXT_KEY => {
-                Self::invalid_key_with_source(
-                    "selected key is not valid for the operation",
-                    source,
-                )
-            },
-            TPM_RC_AUTH_MISSING
-            | TPM_RC_AUTH_UNAVAILABLE
-            | TPM_RC_AUTH_FAIL 
+            TPM_RC_BINDING | TPM_RC_KEY | TPM_RC_SIGN_CONTEXT_KEY => {
+                Self::invalid_key_with_source("selected key is not valid for the operation", source)
+            }
+            TPM_RC_AUTH_FAIL
             | TPM_RC_BAD_AUTH
-            | TPM_RC_NV_AUTHORIZATION
-            | TPM_RC_LOCKOUT
-            | TPM_RC_PP 
+            | TPM_RC_PP
             | TPM_RC_CHANNEL
             | TPM_RC_CHANNEL_KEY
-            | TPM_RC_POLICY 
+            | TPM_RC_POLICY_FAIL
+            | TPM_RC_POLICY_CC
+            | TPM_RC_EXPIRED
+            | TPM_RC_AUTH_MISSING
+            | TPM_RC_AUTH_UNAVAILABLE
+            | TPM_RC_NV_AUTHORIZATION
+            | TPM_RC_LOCKOUT
+            | TPM_RC_POLICY
             | TPM_RC_PCR
-            | TPM_RC_PCR_CHANGED
-            | TPM_RC_POLICY_FAIL 
-            | TPM_RC_POLICY_CC 
-            | TPM_RC_EXPIRED => Self::authorization_failed(source),
+            | TPM_RC_PCR_CHANGED => Self::authorization_failed(source),
             TPM_RC_SIGNATURE => Self::invalid_signature(source),
             TPM_RC_NV_SPACE
             | TPM_RC_OBJECT_MEMORY
@@ -119,11 +110,8 @@ impl Error {
             | TPM_RC_SESSION_HANDLES
             | TPM_RC_OBJECT_HANDLES
             | TPM_RC_TOO_MANY_CONTEXTS => {
-                Self::resource_exhausted_with_source(
-                    "TPM resource exhaused",
-                    source,
-                )
-            },
+                Self::resource_exhausted_with_source("TPM resource exhausted", source)
+            }
             TPM_RC_YIELDED
             | TPM_RC_TESTING
             | TPM_RC_NEEDS_TEST
@@ -135,7 +123,7 @@ impl Error {
                     "TPM does not support the requested operation",
                     source,
                 )
-            },
+            }
             TPM_RC_ASYMMETRIC
             | TPM_RC_HASH
             | TPM_RC_KEY_SIZE
@@ -150,11 +138,8 @@ impl Error {
             | TPM_RC_PARMS
             | TPM_RC_EXT_MU
             | TPM_RC_ONE_SHOT_SIGNATURE => {
-                Self::unsupported_with_source(
-                "TPM does not support the requested value",
-                source,
-                )
-            },
+                Self::unsupported_with_source("TPM does not support the requested value", source)
+            }
             TPM_RC_AUTH_TYPE
             | TPM_RC_BAD_TAG
             | TPM_RC_SEQUENCE
@@ -171,18 +156,13 @@ impl Error {
             | TPM_RC_LOCALITY
             | TPM_RC_REFERENCE_H0..=TPM_RC_REFERENCE_H6
             | TPM_RC_REFERENCE_S0..=TPM_RC_REFERENCE_S6 => {
-                tracing::error!(rc = format_args!("{:#05X}", rc), "unexpected TPM response code");
-                Self::Internal("unexpected error occured")
-            },
+                tracing::error!(
+                    rc = format_args!("{:#05X}", rc.raw()),
+                    "unexpected TPM response code"
+                );
+                Self::Internal("unexpected error occurred")
+            }
             _ => Self::failure(source),
         }
-    }
-}
-
-fn base_rc(rc: TpmRc) -> TpmRc {
-    if rc & TPM_RC_FMT1 != 0 {
-        TPM_RC_FMT1 | (rc & TPM_RC_FMT1_E_MASK)
-    } else {
-        rc
     }
 }
