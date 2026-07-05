816fe7911bcefc57d4187e61abe1d858c7cb0d95
tsuruko12 <tsuruko-12@outlook.com>
2026-07-05 16:43:54 +0900

    refactor: define get_capability_once

--- src/backend/linux/context/capability.rs
+++ src/backend/linux/context/capability.rs
@@ -1,16 +1,22 @@
-use crate::{Result, types::{CapabilityData, TpmCap}};
+use crate::{
+    Error, Result,
+    types::{CapabilityData, TpmCap},
+};
 
 use super::Context;
 
 impl Context {
-    pub(crate) fn read_capability_once(
-        &mut self, 
-        capability: TpmCap, 
+    pub(crate) fn get_capability_once(
+        &mut self,
+        capability: TpmCap,
         property: u32,
         property_count: u32,
     ) -> Result<(bool, CapabilityData)> {
+        let (data, more) = self
+            .ctx
+            .get_capability(capability.into(), property, property_count)
+            .map_err(|e| Error::from_tss_err(e))?;
 
-
-        Ok(())
+        Ok((more, data.try_into()?))
     }
 }
