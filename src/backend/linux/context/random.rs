a7dd637846b1cf285ac75e1c0b16d9533b962f69
tsuruko12 <tsuruko-12@outlook.com>
2026-07-05 16:44:35 +0900

    chore: cleanup code

--- src/backend/linux/context/random.rs
+++ src/backend/linux/context/random.rs
@@ -4,8 +4,7 @@ use super::Context;
 
 impl Context {
     pub(crate) fn get_random_once(&mut self, num_bytes: u16) -> Result<Vec<u8>> {
-        self
-            .ctx
+        self.ctx
             .get_random(num_bytes as usize)
             .map(|digest| digest.value().to_vec())
             .map_err(Error::from_tss_err)
