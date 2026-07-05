e6ccade22f56d3ecb1c850b7dfd23e981da5e68e
tsuruko12 <tsuruko-12@outlook.com>
2026-07-05 16:51:37 +0900

    refactor: implement TryFrom

--- src/types/policy.rs
+++ src/types/policy.rs
@@ -81,6 +81,40 @@ impl From<PcrSlot> for u32 {
     }
 }
 
+impl TryFrom<u8> for PcrSlot {
+    type Error = crate::Error;
+
+    fn try_from(value: u8) -> crate::Result<Self> {
+        match value {
+            0 => Ok(Self::Slot0),
+            1 => Ok(Self::Slot1),
+            2 => Ok(Self::Slot2),
+            3 => Ok(Self::Slot3),
+            4 => Ok(Self::Slot4),
+            5 => Ok(Self::Slot5),
+            6 => Ok(Self::Slot6),
+            7 => Ok(Self::Slot7),
+            8 => Ok(Self::Slot8),
+            9 => Ok(Self::Slot9),
+            10 => Ok(Self::Slot10),
+            11 => Ok(Self::Slot11),
+            12 => Ok(Self::Slot12),
+            13 => Ok(Self::Slot13),
+            14 => Ok(Self::Slot14),
+            15 => Ok(Self::Slot15),
+            16 => Ok(Self::Slot16),
+            17 => Ok(Self::Slot17),
+            18 => Ok(Self::Slot18),
+            19 => Ok(Self::Slot19),
+            20 => Ok(Self::Slot20),
+            21 => Ok(Self::Slot21),
+            22 => Ok(Self::Slot22),
+            23 => Ok(Self::Slot23),
+            _ => Err(crate::Error::Internal("unsupported PCR slot")),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct PcrSelection {
     hash: HashAlgorithm,
@@ -105,6 +139,25 @@ impl PcrSelection {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) struct TpmlPcrSelection {
+    items: Vec<TpmsPcrSelection>,
+}
+
+impl TpmlPcrSelection {
+    pub(crate) fn new(items: Vec<TpmsPcrSelection>) -> Self {
+        Self { items }
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub(crate) fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct TpmsPcrSelection {
     hash: TpmiAlgHash,
     pcr_select: Vec<u8>,
@@ -121,5 +174,9 @@ impl TpmsPcrSelection {
 
     pub(crate) fn pcr_select(&self) -> &[u8] {
         &self.pcr_select
+    }
+
+    pub(crate) fn size_of_select(&self) -> usize {
+        self.pcr_select.len()
     }
 }
