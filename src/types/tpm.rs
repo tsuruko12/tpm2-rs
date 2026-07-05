764a607f622562588b0e1d924a197e8f773a6246
tsuruko12 <tsuruko-12@outlook.com>
2026-07-05 16:52:01 +0900

    refator: define TpmlCc

--- src/types/tpm.rs
+++ src/types/tpm.rs
@@ -1,1 +1,20 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) struct TpmlCc {
+    items: Vec<TpmCc>,
+}
+
+impl TpmlCc {
+    pub(crate) fn new(items: Vec<TpmCc>) -> Self {
+        Self { items }
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub(crate) fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
+
 pub(crate) type TpmCc = u32;
