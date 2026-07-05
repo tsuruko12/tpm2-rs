b28d02accca1f7d07ccd3850172a1e5c2bdb94e5
tsuruko12 <tsuruko-12@outlook.com>
2026-07-05 16:46:40 +0900

    refactor: implement TryFrom

--- src/backend/linux/types/capability.rs
+++ src/backend/linux/types/capability.rs
@@ -1,6 +1,9 @@
-use tss_esapi::{constants::CapabilityType};
+use tss_esapi::{constants::CapabilityType, structures::CapabilityData as EsapiCapabilityData};
 
-use crate::types::TpmCap;
+use crate::{
+    Error, Result,
+    types::{CapabilityData, TpmCap},
+};
 
 impl From<TpmCap> for CapabilityType {
     fn from(value: TpmCap) -> Self {
@@ -14,6 +17,25 @@ impl From<TpmCap> for CapabilityType {
             TpmCap::TpmProperties => Self::TpmProperties,
             TpmCap::PcrProperties => Self::PcrProperties,
             TpmCap::ECCCurves => Self::EccCurves,
+        }
+    }
+}
+
+impl TryFrom<EsapiCapabilityData> for CapabilityData {
+    type Error = Error;
+
+    fn try_from(value: EsapiCapabilityData) -> Result<Self> {
+        match value {
+            EsapiCapabilityData::Algorithms(list) => Ok(Self::Algs(list.try_into()?)),
+            EsapiCapabilityData::Handles(list) => Ok(Self::Handles(list.into())),
+            EsapiCapabilityData::Commands(list) => Ok(Self::Commands(list.into())),
+            EsapiCapabilityData::PpCommands(list) => Ok(Self::PpCommands(list.into())),
+            EsapiCapabilityData::AuditCommands(list) => Ok(Self::AuditCommands(list.into())),
+            EsapiCapabilityData::AssignedPcr(list) => Ok(Self::Pcrs(list.into())),
+            EsapiCapabilityData::TpmProperties(list) => Ok(Self::TpmProperties(list.try_into()?)),
+            EsapiCapabilityData::PcrProperties(list) => Ok(Self::PcrProperties(list.try_into()?)),
+            EsapiCapabilityData::EccCurves(list) => Ok(Self::EccCurves(list.try_into()?)),
+            _ => unreachable!("unexpected capability data"),
         }
     }
 }
