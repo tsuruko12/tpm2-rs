12de6dc4e3aaaa3c1ca300e6438ea393e0bbb6d4
tsuruko12 <tsuruko12@users.noreply.github.com>
2026-08-06 16:40:20 +0900

    ""

--- src/backend/windows/context/handle.rs
+++ src/backend/windows/context/handle.rs
@@ -27,7 +27,7 @@ impl Context {
         session_salt_key: Option<TpmiDhObject>,
         search_end: Option<TpmiDhPersistent>,
     ) -> Result<()> {
-        assert_eq!(obj_handle.is_transient(), true);
+        println!("evict handle: {}", obj_handle.raw());
         let command_code = TpmCc::EVICT_CONTROL;
         let owner_handle = TpmiRhProvision::OWNER;
         let handle_name = self.read_object_name(obj_handle)?;
