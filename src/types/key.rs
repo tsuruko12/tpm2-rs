77a0e2411b27fa4c602ed759c68900d6ac04de59
tsuruko12 <tsuruko-12@outlook.com>
2026-08-07 14:44:11 +0900

    chore: define Tpm2bName

--- src/types/key.rs
+++ src/types/key.rs
@@ -1,13 +1,71 @@
 #[cfg(target_os = "windows")]
 use crate::types::TpmiDhObject;
 use crate::{
-    macros::{tpm2b_bytes_type, tpm2b_secret_type}, types::{Authorization, Tpm2bPublic},
+    Error, Result, 
+    macros::tpm2b_secret_type, 
+    types::{Authorization, Tpm2bDigest, Tpm2bPublic, TpmAlgId, TpmHandle}
 };
 #[cfg(target_os = "linux")]
 use tss_esapi::handles::KeyHandle;
 
 tpm2b_secret_type!(Tpm2bPrivate);
-tpm2b_bytes_type!(Tpm2bName);
+
+#[derive(Debug, Default, Clone)]
+pub(crate) struct Tpm2bName(Vec<u8>);
+
+impl Tpm2bName {
+    const NO_NAME_SIZE: usize = 0;
+    const HANDLE_SIZE: usize = size_of::<TpmHandle>();
+    const MAX_SIZE: usize = size_of::<TpmAlgId>() + Tpm2bDigest::MAX_SIZE; // TPMT_HA
+
+    pub(crate) fn into_bytes(self) -> Vec<u8> {
+        self.0
+    }
+
+    pub(crate) fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub(crate) fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl TryFrom<Vec<u8>> for Tpm2bName {
+    type Error = Error;
+
+    fn try_from(value: Vec<u8>) -> Result<Self> {
+        let size = value.len();
+
+        if size == Self::NO_NAME_SIZE 
+            || size == Self::HANDLE_SIZE 
+            || size == Self::MAX_SIZE {
+            Ok(Self(value))
+        } else {
+            Err(Error::conversion::<Vec<u8>, Tpm2bName>(None))
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for Tpm2bName {
+    type Error = Error;
+
+    fn try_from(value: &[u8]) -> Result<Self> {
+        let size = value.len();
+
+        if size == Self::NO_NAME_SIZE 
+            || size == Self::HANDLE_SIZE 
+            || size == Self::MAX_SIZE {
+            Ok(Self(value.into()))
+        } else {
+            Err(Error::conversion::<Vec<u8>, Tpm2bName>(None))
+        }
+    }
+}
 
 #[cfg(target_os = "linux")]
 pub(crate) type LoadedObjectHandle = KeyHandle;
